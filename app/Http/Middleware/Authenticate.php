51abe9d3299a34cb5769eed3b31c9d9c9274d3d6
Jean <jeangiordani290@gmail.com>
2021-09-09 20:47:23 -0300

    add category actions

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,7 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if (!$request->expectsJson()) {
+            // return route('auth.login');
             return route('login');
         }
     }
